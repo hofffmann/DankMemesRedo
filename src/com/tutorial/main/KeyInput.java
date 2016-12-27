ca642eed8c303254027c34d58a6e41d46d0ccc77
Wiatt <Wiatt@Ballernar>
2016-12-27 01:20:41 -0600

    updated speeds

--- src/com/tutorial/main/KeyInput.java
+++ src/com/tutorial/main/KeyInput.java
@@ -20,18 +20,18 @@ public class KeyInput extends KeyAdapter {
 			if(tempObject.getId() == ID.Player) {
 				//Key Events for Player 1
 				
-				if(key == KeyEvent.VK_W) tempObject.setVelY(-5);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(5);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(5);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(-5);
+				if(key == KeyEvent.VK_W) tempObject.setVelY(-1);
+				if(key == KeyEvent.VK_S) tempObject.setVelY(1);
+				if(key == KeyEvent.VK_D) tempObject.setVelX(1);
+				if(key == KeyEvent.VK_A) tempObject.setVelX(-1);
 			}
 			if(tempObject.getId() == ID.Player2) {
 				//Key Events for Player 2
 				
-				if(key == KeyEvent.VK_UP) tempObject.setVelY(-5);
-				if(key == KeyEvent.VK_DOWN) tempObject.setVelY(5);
-				if(key == KeyEvent.VK_RIGHT) tempObject.setVelX(5);
-				if(key == KeyEvent.VK_LEFT) tempObject.setVelX(-5);
+				if(key == KeyEvent.VK_UP) tempObject.setVelY(-1);
+				if(key == KeyEvent.VK_DOWN) tempObject.setVelY(1);
+				if(key == KeyEvent.VK_RIGHT) tempObject.setVelX(1);
+				if(key == KeyEvent.VK_LEFT) tempObject.setVelX(-1);
 			}
 			
 			
